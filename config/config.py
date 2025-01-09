ef34bc80c53f57f7da8c67bb580506fd8151fca9
gauravverma9795 <gauravverma9795@gmail.com>
2025-01-10 01:57:39 +0530

    initial commit

---
+++ config/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+    TWITTER_USERNAME = "@gauravverma9795"
+    TWITTER_PASSWORD = "sdgg@1234"
+    MONGO_URI = "mongodb+srv://gauravverma9795:jftI3CuPEWaqO0Bk@cluster0.umizl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
